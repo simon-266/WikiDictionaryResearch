d3dac3e51d8e75202ea3b981df9ba3ad7a47e096
simon-266 <160402589+simon-266@users.noreply.github.com>
2024-07-06 15:10:53 +0200

    Final Submit
    Improved extraction.py
    Added Jupiter Notebook
    Added the Frequency as a csv and a db file
    Added the list of articles used for creating the csv and db file

--- extraction.py
+++ extraction.py
@@ -11,14 +11,18 @@ import warnings
 warnings.catch_warnings()
 warnings.simplefilter("ignore")
 
+#open file for saving the entire list of articles which got used
+list_of_articles = open("ressources/list_of_articles.txt", 'a')
+
 def fetch_and_process_article():
 
     #Takes a random Wikipedia article, processes the text, count words and returns a counter object with word counts.
     wikipedia.set_lang("en")
     try:
         random_article = wikipedia.random()
-        random_article = wikipedia.page(random_article)
-        text = random_article.content
+        article = wikipedia.page(random_article)
+        text = article.content
+        list_of_articles.write(random_article + "\n")
     except (wikipedia.exceptions.PageError, wikipedia.exceptions.DisambiguationError):
         return None
     
@@ -45,6 +49,8 @@ def process_articles_in_batches(batch_size, total_runs):
         batch_size: Number of articles to process in each batch
         total_runs: Total number of articles to process.
     """
+
+    #used Ai for understanding some concepts of Multithreading
     aggregated_counts = Counter()
     with ThreadPoolExecutor() as executor:
         for i in range(0, total_runs, batch_size):
@@ -58,28 +64,43 @@ def process_articles_in_batches(batch_size, total_runs):
 start = time.time()
 
 #To make the process faster but use more memory increase the batch size
-total_runs = 10_000
-batch_size = 100
+total_runs = 15_000
+batch_size = 5000
 #Executes the multithreading function
 final_counts = process_articles_in_batches(batch_size, total_runs)
+
 
 #Convert to DataFrame
 final_result = pd.DataFrame(final_counts.items(), columns=['word', 'frequency'])
 
+import enchant
+d = enchant.Dict("en_US")
+def is_english_word(word):
+    return d.check(word)
+
+#Filtering non english words
+final_result = final_result[final_result['word'].apply(is_english_word)]
+
 #Calculate percentage of each word
 total_words = final_result['frequency'].sum()
-final_result['frequency in %'] = round(final_result['frequency'] / total_words * 100, 6)
-final_result = final_result.append({'word': '_total', 'frequency': total_words, 'frequency in %': 100}, ignore_index=True)
+final_result['frequency_in_percent'] = round(final_result['frequency'] / total_words * 100, 6)
+
+#Save to SQLite database
+conn = sqlite3.connect('ressources/word_frequency.db')
+final_result.to_sql('word_frequency', conn, if_exists='replace', index=False)
+conn.close()
+
+#adding a row with the total values
+final_result = final_result.append({'word': '_total', 'frequency': total_words, 'frequency_in_percent': final_result['frequency_in_percent'].sum()}, ignore_index=True)
 
 #Save to CSV
 final_result['frequency'] = final_result['frequency'].astype(int)
 final_result = final_result.sort_values(by="frequency", ascending=False)
-final_result.to_csv('word_frequency.csv', index=False)
+final_result.to_csv('ressources/word_frequency.csv', index=False)
 
-#Save to SQLite database
-conn = sqlite3.connect('word_frequency.db')
-final_result.to_sql('word_frequency', conn, if_exists='replace', index=False)
-conn.close()
-
+list_of_articles.close()
 end = time.time()
+
 print(f"Finished! {end-start}s")
+
+#Time elapsed:  1104.005 seconds = ~18.4mins
